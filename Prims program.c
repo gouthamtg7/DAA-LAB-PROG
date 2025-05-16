5ec01ef841906eb7774824fd444e86a97830daf2
gouthamtg7 <gouthamtaranagar.ai23@bmsce.ac.in>
2025-05-16 10:24:41 +0530

    Update Prims program.c

--- Prims program.c
+++ Prims program.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <limits.h>
 
-
 #define V 5
-
 
 int minKey(int key[], int mstSet[]) {
     int min = INT_MAX, min_index;
@@ -16,7 +14,6 @@ int minKey(int key[], int mstSet[]) {
     return min_index;
 }
 
-
 void printMST(int parent[], int graph[V][V]) {
     printf("Edge \tWeight\n");
     for (int i = 1; i < V; i++) {
@@ -24,22 +21,44 @@ void printMST(int parent[], int graph[V][V]) {
     }
 }
 
-
 void primMST(int graph[V][V]) {
-    int parent[V];  // Array to store the constructed MST
-    int key[V];     // Used to select the minimum weight edge
-    int mstSet[V];  // To represent the set of vertices included in the MST
-
+    int parent[V];
+    int key[V];
+    int mstSet[V];
 
     for (int i = 0; i < V; i++) {
         key[i] = INT_MAX;
         mstSet[i] = 0;
     }
 
- 
-    key[0] = 0;  // Starting from vertex 0
-    parent[0] = -1;  // First node has no parent
+    key[0] = 0;
+    parent[0] = -1;
 
-    
     for (int count = 0; count < V - 1; count++) {
-     
+        int u = minKey(key, mstSet);
+        mstSet[u] = 1;
+
+        for (int v = 0; v < V; v++) {
+            if (graph[u][v] && mstSet[v] == 0 && graph[u][v] < key[v]) {
+                parent[v] = u;
+                key[v] = graph[u][v];
+            }
+        }
+    }
+
+    printMST(parent, graph);
+}
+
+int main() {
+    int graph[V][V] = {
+        {0, 2, 0, 6, 0},
+        {2, 0, 3, 8, 5},
+        {0, 3, 0, 0, 7},
+        {6, 8, 0, 0, 9},
+        {0, 5, 7, 9, 0}
+    };
+
+    primMST(graph);
+
+    return 0;
+}
